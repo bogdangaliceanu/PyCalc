e595e59fc0addf188cf653667b2c40c88aa6261f
Bogdan <bogdan.galiceanu@yahoo.com>
2017-07-02 18:18:48 +0300

    give each calculation its own route

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,9 +1,9 @@
 try:
-    from calculations import *
+    import calculations as calc
     from operation import Operation
     from inmemorystorage import InMemoryStorage
 except:
-    from src.calculations import *
+    import src.calculations as calc
     from src.operation import Operation
     from src.inmemorystorage import InMemoryStorage
 from flask import Flask, Response
@@ -14,25 +14,28 @@ import uuid
 app = Flask(__name__)
 app.config['STORAGE'] = InMemoryStorage()
 
-@app.route("/execute/<operand1>/<calculation>/<operand2>")
-def execute(operand1, calculation, operand2):
-    op1, op2 = float(operand1), float(operand2)
-
-    if calculation == "addition":
-        result = add(op1, op2)
-    elif calculation == "subtraction":
-        result = subtract(op1, op2)
-    elif calculation == "multiplication":
-        result = multiply(op1, op2)
-    elif calculation == "division":
-        result = divide(op1, op2)
-    else:
-        raise Exception('calculation not recognized (' + calculation + ')')
-
-    operation = Operation(op1, op2, calculation, result, time(), uuid.uuid4())
+def handle(calculationFunc, operand1, operand2, calculationName):
+    result = calculationFunc(operand1, operand2)
+    operation = Operation(operand1, operand2, calculationName, result, time(), uuid.uuid4())
     app.config['STORAGE'].persist(operation)
 
     return Response(str(result), mimetype='text/plain')
+
+@app.route("/add/<operand1>/<operand2>")
+def add(operand1, operand2):
+    return handle(calc.add, float(operand1), float(operand2), 'addition')
+
+@app.route("/subtract/<operand1>/<operand2>")
+def subtract(operand1, operand2):
+    return handle(calc.subtract, float(operand1), float(operand2), 'subtraction')
+
+@app.route("/multiply/<operand1>/<operand2>")
+def multiply(operand1, operand2):
+    return handle(calc.multiply, float(operand1), float(operand2), 'multiplication')
+
+@app.route("/divide/<operand1>/<operand2>")
+def divide(operand1, operand2):
+    return handle(calc.divide, float(operand1), float(operand2), 'division')
 
 @app.route("/history")
 def get_history():
--- tests/endpointtests.py
+++ tests/endpointtests.py
@@ -11,23 +11,23 @@ class EndpointTests(TestCase):
         self.app = app.test_client()
 
     def test_addition(self):
-        response = self.app.get('/execute/2.8/addition/9.2')
+        response = self.app.get('/add/2.8/9.2')
         self.assertEqual(12.0, float(response.data))
 
     def test_subtraction(self):
-        response = self.app.get('/execute/2/subtraction/9')
+        response = self.app.get('/subtract/2/9')
         self.assertEqual(-7.0, float(response.data))
 
     def test_multiplication(self):
-        response = self.app.get('/execute/2/multiplication/9')
+        response = self.app.get('/multiply/2/9')
         self.assertEqual(18.0, float(response.data))
 
     def test_division(self):
-        response = self.app.get('/execute/7.5/division/2')
+        response = self.app.get('/divide/7.5/2')
         self.assertEqual(3.75, float(response.data))
 
     def test_history_is_kept(self):
-        self.app.get('/execute/2/addition/9')
+        self.app.get('/add/2/9')
         response = self.app.get('/history')
         [operation] = json.loads(response.data)
         self.assertEqual(11.0, operation['result'])
@@ -36,7 +36,7 @@ class EndpointTests(TestCase):
         self.assertEqual('addition', operation['calculation'])
 
     def test_history_can_be_cleared(self):
-        self.app.get('/execute/2/addition/9')
+        self.app.get('/add/2/9')
         self.app.delete('/history')
         response = self.app.get('/history')
         self.assertListEqual([], json.loads(response.data))
