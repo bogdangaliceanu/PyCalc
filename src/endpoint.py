a33925c0d0ca6484fe2477f9d631ba41b545feab
Bogdan <bogdan.galiceanu@yahoo.com>
2017-07-02 16:02:01 +0300

    add execute to endpoint

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,4 +1,3 @@
-from calculations.addition import add
 # from operation import Operation
 # from filestorage import FileStorage
 # import time
@@ -15,13 +14,22 @@ from calculations.addition import add
 # allOps = storage.retrieve_all()
 # o2 = storage.retrieve(o.uid)
 
+from calculations import *
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/execute/<operand1>/<calculation>/<operand2>")
 def execute(operand1, calculation, operand2):
     if calculation == "addition":
-        result = add
+        return str(add(float(operand1), float(operand2)))
+
+# @app.route("/history")
+# def get_history():
+
+
+# @app.route("/history", methods=["DELETE"])
+# def clear_history():
+
 
 if __name__ == "__main__":
     app.run()
